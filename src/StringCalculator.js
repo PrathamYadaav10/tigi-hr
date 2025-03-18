0eca3f15618dcbbb0f52c21b28e2b7b50dccd7b2
karambirsingh2520 <karambirsingh2520@gmail.com>
2025-03-19 01:16:19 +0530

    Basic structure, passing the empty string test.

---
+++ src/StringCalculator.js
@@ -0,0 +1,16 @@
+// src/StringCalculator.js
+import React from 'react';
+
+const StringCalculator = () => {
+  const add = (numbers) => {
+    return 0;
+  };
+
+  return (
+    <div>
+      <h1>String Calculator</h1>
+    </div>
+  );
+};
+
+export default StringCalculator;
